b20515aaa9688da28a970b5b0d672ee2e040c28c
Michael S. Klishin <michael@novemberain.com>
2011-11-18 20:44:10 +0400

    Implement welle.buckets/list-buckets

--- src/welle/buckets.clj
+++ src/welle/buckets.clj
@@ -9,9 +9,13 @@
   ^Bucket [^String bucket-name &{ :keys [allow-siblings, last-write-wins, n-val, backend, small-vclock, big-vclock, young-vclock, old-vclock, r, w] }]
   (let [write-bucket (.createBucket ^IRiakClient *riak-client* ^String bucket-name)]
     (when allow-siblings
-      (.allowSiblings ^WriteBucket write-bucket allow-siblings))
+      (.allowSiblings  write-bucket allow-siblings))
     (when last-write-wins
-      (.lastWriteWins ^WriteBucket write-bucket last-write-wins))
+      (.lastWriteWins write-bucket last-write-wins))
     (when n-val
-      (.nVal ^WriteBucket write-bucket n-val))
+      (.nVal write-bucket n-val))
     (.execute write-bucket)))
+
+(defn list-buckets
+  []
+  (set (.listBuckets ^IRiakClient *riak-client*)))
