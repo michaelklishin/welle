2a79505404621bb37515539005e092754f91866f
Michael S. Klishin <michael@novemberain.com>
2011-12-02 15:05:53 +0400

    Upgrade to Riak Java client 1.0.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.novemberain/welle "0.1.0-SNAPSHOT"
   :description "An experimental wrapper around Riak Java client"
   :dependencies [[org.clojure/clojure        "1.3.0"]
-                 [com.basho.riak/riak-client "1.0.1"]]
+                 [com.basho.riak/riak-client "1.0.2"]]
   :repositories { "sonatype"
                  {:url "http://oss.sonatype.org/content/repositories/releases"
                   :snapshots false
