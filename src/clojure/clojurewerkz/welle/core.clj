970ecb2598711cda1e9955853d92ec54fd912e95
Michael S. Klishin <michael@novemberain.com>
2012-04-16 13:39:23 +0400

    Switch welle.core to RawClient

--- src/clojure/clojurewerkz/welle/core.clj
+++ src/clojure/clojurewerkz/welle/core.clj
@@ -1,5 +1,7 @@
 (ns clojurewerkz.welle.core
-  (:import [com.basho.riak.client IRiakClient RiakFactory]))
+  (:import com.basho.riak.client.raw.RawClient
+           com.basho.riak.client.raw.pbc.PBClientAdapter
+           com.basho.riak.client.raw.http.HTTPClientAdapter))
 
 
 
@@ -7,22 +9,20 @@
 ;; API
 ;;
 
-(def ^:dynamic *riak-host* "127.0.0.1")
-(def ^:dynamic *riak-port* 8087)
+(def ^{:private true :const true} default-host "127.0.0.1")
+(def ^{:private true :const true} default-port 8087)
 
-(def ^:dynamic *riak-url* "http://127.0.0.1:8098/riak")
+(def ^{:private true :const true} default-url "http://127.0.0.1:8098/riak")
 
-(declare ^:dynamic ^IRiakClient *riak-client*)
+(def ^:dynamic ^RawClient *riak-client*)
 
 
-(defn connect
-  (^IRiakClient []
-                (connect *riak-url*))
-  (^IRiakClient [^String url]
-                (RiakFactory/httpClient ^String url))
-  (^IRiakClient [^String url ^String client-id]
-                (doto (RiakFactory/httpClient ^String url)
-                  (.setClientId (.getBytes client-id)))))
+(defn ^com.basho.riak.client.raw.RawClient
+  connect
+  ([]
+     (connect default-url))
+  ([^String url]
+     (HTTPClientAdapter. (com.basho.riak.client.http.RiakClient. ^String url))))
 
 (defn connect!
   ([]
@@ -32,13 +32,13 @@
   ([^String url ^String client-id]
      (alter-var-root (var *riak-client*) (constantly (connect url client-id)))))
 
-(defn connect-via-pcb
-  (^IRiakClient []
-                (connect *riak-host* *riak-port*))
-  (^IRiakClient [^String host ^long port]
-                (RiakFactory/pbcClient host port)))
+(defn connect-via-pb
+  ([]
+     (connect-via-pb default-host default-port))
+  ([^String host ^long port]
+     (PBClientAdapter. (com.basho.riak.pbc.RiakClient. host port))))
 
-(defn connect-via-pcb!
+(defn connect-via-pb!
   ([]
      (alter-var-root (var *riak-client*) (constantly (connect))))
   ([host port]
@@ -48,11 +48,11 @@
 (defn ping
   ([]
      (.ping *riak-client*))
-  ([^IRiakClient client]
+  ([^RawClient client]
      (.ping client)))
 
 (defn shutdown
   ([]
      (.shutdown *riak-client*))
-  ([^IRiakClient client]
+  ([^RawClient client]
      (.shutdown client)))
--- test/clojurewerkz/welle/test/core_test.clj
+++ test/clojurewerkz/welle/test/core_test.clj
@@ -1,20 +1,20 @@
 (ns clojurewerkz.welle.test.core-test
-  (:import [com.basho.riak.client IRiakClient])
+  (:import com.basho.riak.client.raw.RawClient)
   (:require [clojurewerkz.welle.core :as wc])
   (:use clojure.test))
 
 (set! *warn-on-reflection* true)
 
-(deftest connect-using-pcb-client-and-default-host-and-port
+(deftest connect-using-http-client-and-default-host-and-port
   (let [client (wc/connect)]
     (dotimes [x 10]
       (.ping client)
       (wc/ping client)
       (wc/shutdown client))))
 
-(deftest connect-using-pcb-client-default-host-and-port-and-default-client
+(deftest connect-using-http-client-default-host-and-port-and-default-client
   (wc/connect!)
   (dotimes [x 10]
-    (.ping ^IRiakClient wc/*riak-client*)
+    (.ping ^RawClient wc/*riak-client*)
     (wc/ping)
     (wc/shutdown)))
