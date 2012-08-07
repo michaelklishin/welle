b8e94d59720503671581987add2e7e4af7537efb
Michael S. Klishin <michael@novemberain.com>
2012-08-07 22:58:41 +0400

    Another project.clj update, clojure.core.cache 0.6.2 is not available from Central

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.novemberain/welle "1.2.0-SNAPSHOT"
   :description "Welle is an expressive Clojure client for Riak with batteries included"
-  :url "http://clojureriak.info"  
+  :url "http://clojureriak.info"
   :license {:name "Eclipse Public License"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure        "1.3.0"]
@@ -12,14 +12,14 @@
   :profiles       {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
                    :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
                    :dev {:resource-paths ["test/resources"]
-                         :dependencies [[org.clojure/core.cache "0.6.2" :exclusions [org.clojure/clojure]]
+                         :dependencies [[org.clojure/core.cache "0.6.1" :exclusions [org.clojure/clojure]]
                                         [ring/ring-core         "1.1.1"]]
                          :plugins [[codox "0.6.1"]]
                          :codox {:sources ["src/clojure"]
                                  :output-dir "doc/api"}}}
   :mailing-list {:name "clojure-riak"
                  :archive "https://groups.google.com/group/clojure-riak"
-                 :post "clojure-riak@googlegroups.com"}  
+                 :post "clojure-riak@googlegroups.com"}
   :aliases        {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]}
   :test-selectors {:focus   :focus
                    :2i      :2i
@@ -27,7 +27,9 @@
                    :mr      :mr
                    :links   :links
                    :search  :search
-                   :default (constantly true)}
+                   ;; as in, edge Riak features
+                   ::edge-features :edge-features
+                   :all     (constantly true)}
   :repositories   {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                                :snapshots false
                                :releases {:checksum :fail :update :always}}
