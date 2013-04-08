242587679cfc69c2cc73ae5ec3602b06574c70c0
Michael Klishin <michael@defprotocol.org>
2013-04-08 21:59:12 +0400

    1.5.0-beta3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/welle "1.5.0-beta3-SNAPSHOT"
+(defproject com.novemberain/welle "1.5.0-beta3"
   :description "Welle is an expressive Clojure client for Riak with batteries included"
   :url "http://clojureriak.info"
   :license {:name "Eclipse Public License"}
