b769e557e61df05423db54af6f4701967cce937b
Michael Klishin <michael@defprotocol.org>
2013-04-19 23:21:42 +0400

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/welle "1.5.0"
+(defproject com.novemberain/welle "1.6.0-SNAPSHOT"
   :description "Welle is an expressive Clojure client for Riak with batteries included"
   :url "http://clojureriak.info"
   :license {:name "Eclipse Public License"}
