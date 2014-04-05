9ed542f8c8385fd519904aed210873ca2136689f
Michael Klishin <michael@clojurewerkz.org>
2014-04-05 11:38:21 +0400

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/welle "2.0.0"
+(defproject com.novemberain/welle "2.1.0-SNAPSHOT"
   :description "Welle is an expressive Clojure client for Riak with batteries included"
   :url "http://clojureriak.info"
   :license {:name "Eclipse Public License"}
