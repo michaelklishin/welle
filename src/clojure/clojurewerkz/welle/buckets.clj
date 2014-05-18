95334a4a468223bcd9e129a6575fc2cbcd1f7a88
Michael Klishin <michael@clojurewerkz.org>
2014-05-18 06:29:34 +0400

    Adapt welle.buckets

--- src/clojure/clojurewerkz/welle/buckets.clj
+++ src/clojure/clojurewerkz/welle/buckets.clj
@@ -11,7 +11,8 @@
   (:refer-clojure :exclude [list])
   (:require [clojurewerkz.welle.core :refer :all]
             [clojurewerkz.welle.conversion :refer :all])
-  (:import [com.basho.riak.client IRiakClient IRiakObject]
+  (:import com.basho.riak.client.raw.RawClient
+           [com.basho.riak.client IRiakClient IRiakObject]
            [com.basho.riak.client.bucket Bucket WriteBucket]
            [com.basho.riak.client.http.response BucketResponse ListBucketsResponse]
            [com.basho.riak.client.operations StoreObject FetchObject]
@@ -26,9 +27,9 @@
 
 (defn fetch
   "Fetches bucket properties"
-  [^String bucket-name]
+  [^RawClient client ^String bucket-name]
   (merge {:name bucket-name}
-         (from-bucket-properties (.fetchBucket *riak-client* bucket-name))))
+         (from-bucket-properties (.fetchBucket client bucket-name))))
 
 (defn update
   "Updates bucket properties.
@@ -56,15 +57,17 @@
    * big-vclock
    * young-vclock
    * old-vclock"
-  [^String bucket-name &{ :keys [allow-siblings last-write-wins n-val ^String backend
-                                 small-vclock big-vclock young-vclock old-vclock
-                                 r pr w dw pw rw
-                                 ^Boolean not-found-ok ^Boolean basic-quorum ^Boolean enable-search
-                                 pre-commit-hooks
-                                 post-commit-hooks] :as options}]
-  (.updateBucket *riak-client* bucket-name (to-bucket-properties (or options {})))
-  (merge {:name bucket-name}
-         (from-bucket-properties (.fetchBucket *riak-client* bucket-name))))
+  ([^RawClient client ^String bucket-name]
+     (update client bucket-name {}))
+  ([^RawClient client ^String bucket-name { :keys [allow-siblings last-write-wins n-val ^String backend
+                                                   small-vclock big-vclock young-vclock old-vclock
+                                                   r pr w dw pw rw
+                                                   ^Boolean not-found-ok ^Boolean basic-quorum ^Boolean enable-search
+                                                   pre-commit-hooks
+                                                   post-commit-hooks] :as options}]
+     (.updateBucket client bucket-name (to-bucket-properties (or options {})))
+     (merge {:name bucket-name}
+            (from-bucket-properties (.fetchBucket client bucket-name)))))
 
 (defn ^{:deprecated true} create
   "The same as update. This name reveals the intent a bit better in some cases.
@@ -74,12 +77,12 @@
 
 (defn list
   "Returns buckets in the cluster as a set"
-  []
-  (set (.listBuckets *riak-client*)))
+  [^RawClient client]
+  (set (.listBuckets client)))
 
 
 (defn keys-in
   "Returns list of keys in the bucket. With any non-trivial number of keys, this is a VERY EXPENSIVE operation
    and typically should be avoided"
-  [^String bucket-name]
-  (.listKeys *riak-client* bucket-name))
+  [^RawClient client ^String bucket-name]
+  (.listKeys client bucket-name))
