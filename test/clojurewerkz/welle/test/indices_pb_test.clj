e2200f83dfdf41b2d8df7d6b712422b9dd249264
Michael Klishin <michael@clojurewerkz.org>
2014-05-18 07:48:19 +0400

    Fix destructuring in indices_pb_test

--- test/clojurewerkz/welle/test/indices_pb_test.clj
+++ test/clojurewerkz/welle/test/indices_pb_test.clj
@@ -8,18 +8,18 @@
   (:import java.util.UUID
            com.basho.riak.client.http.util.Constants))
 
-(let [conn (wc/connect-via-pb)]
-  (deftest ^{:2i true :edge-features true} test-basic-index-query-with-a-single-string-value-over-pb
-    (let [bucket-name "clojurewerkz.welle.test.indices-pb-test"
-          bucket      (wb/update conn bucket-name)
-          k           (str (UUID/randomUUID))
-          v           (.getBytes "value")
-          indexes     {:email #{"johndoe@example.com" "timsmith@example.com"}}
-          stored      (kv/store conn bucket-name k v {:indexes indexes :content-type Constants/CTYPE_OCTET_STREAM})
-          [idx-key]   (kv/index-query conn bucket-name :email "johndoe@example.com")
-          [fetched]   (kv/fetch conn bucket-name idx-key)]
-      (is (:indexes fetched))
-      (is (= (String. ^bytes (:value fetched))
-             (String. ^bytes v)))
-      (is (= (:indexes fetched) indexes))
-      (kv/delete conn bucket-name k))))
+(deftest ^{:2i true :edge-features true} test-basic-index-query-with-a-single-string-value-over-pb
+  (let [conn        (wc/connect-via-pb)
+        bucket-name "clojurewerkz.welle.test.indices-pb-test"
+        bucket      (wb/update conn bucket-name)
+        k           (str (UUID/randomUUID))
+        v           (.getBytes "value")
+        indexes     {:email #{"johndoe@example.com" "timsmith@example.com"}}
+        stored      (kv/store conn bucket-name k v {:indexes indexes :content-type Constants/CTYPE_OCTET_STREAM})
+        [idx-key]   (kv/index-query conn bucket-name :email "johndoe@example.com")
+        [fetched]   (:result (kv/fetch conn bucket-name idx-key))]
+    (is (:indexes fetched))
+    (is (= (String. ^bytes (:value fetched))
+           (String. ^bytes v)))
+    (is (= (:indexes fetched) indexes))
+    (kv/delete conn bucket-name k)))
