6b2219aa9c7aa47df1e9db7389e7341f5ef88851
Michael Klishin <michael@clojurewerkz.org>
2014-05-18 07:42:24 +0400

    search_test now passes

--- test/clojurewerkz/welle/test/search_test.clj
+++ test/clojurewerkz/welle/test/search_test.clj
@@ -7,19 +7,20 @@
             [clojurewerkz.welle.testkit :refer [drain]])
   (:import  com.basho.riak.client.http.util.Constants))
 
-(wc/connect!)
+
 
 (deftest ^{:search true} test-term-query-via-the-solr-api
-  (let [bucket-name "clojurewerkz.welle.solr.tweets"
-        bucket      (wb/update bucket-name :last-write-wins true :enable-search true)
+  (let [conn        (wc/connect)
+        bucket-name "clojurewerkz.welle.solr.tweets"
+        bucket      (wb/update conn bucket-name {:last-write-wins true :enable-search true})
         doc         {:username  "clojurewerkz"
                      :text      "Elastisch beta3 is out, several more @elasticsearch features supported github.com/clojurewerkz/elastisch, improved docs http://clojureelasticsearch.info #clojure"
                      :timestamp "20120802T101232+0100"
                      :id        1}]
-    (drain bucket-name)
-    (kv/store bucket-name "a-key" doc :content-type "application/json")
-    (let [result (wsolr/search bucket-name "username:clojurewerkz")
+    (drain conn bucket-name)
+    (kv/store conn bucket-name "a-key" doc {:content-type "application/json"})
+    (let [result (wsolr/search conn bucket-name "username:clojurewerkz")
           hits   (wsolr/hits-from result)]
       (is (= "a-key" (-> hits first :id)))
       (is (> (count hits) 0)))
-    (drain bucket-name)))
+    (drain conn bucket-name)))
